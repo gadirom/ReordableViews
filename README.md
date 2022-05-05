47d5f1bdaf74f678f141f185f4a7f71e777c1661
Roman Gaditskiy <51689121+gadirom@users.noreply.github.com>
2022-05-05 15:36:07 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReordableViews
+
+A description of this package.
---
+++ Tests/ReordableViewsTests/ReordableViewsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ReordableViews
+
+final class ReordableViewsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ReordableViews().text, "Hello, World!")
+    }
+}
