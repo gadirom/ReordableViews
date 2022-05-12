8dccd4004c9c0558452826126a8606425e7dceb4
Roman Gaditskiy <51689121+gadirom@users.noreply.github.com>
2022-05-12 22:41:42 +0300

    container dependency to v1.0.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/gadirom/ContainerGeometry", from: "1.0.0"),
+        .package(url: "https://github.com/gadirom/ContainerGeometry", from: "1.0.1"),
         .package(url: "https://github.com/gadirom/CGMath", from: "1.0.0"),
     ],
     targets: [
